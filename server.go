db533801449d7dd95008d5493aa526d789fe30eb
Keftcha <alban.dewilde@gmail.com>
2020-11-08 00:54:43 +0100

    Display images

--- server.go
+++ server.go
@@ -9,6 +9,14 @@ import (
 func main() {
 	http.HandleFunc("/", gallery)
 
+	http.Handle(
+		"/pics/",
+		http.StripPrefix(
+			"/pics/",
+			http.FileServer(http.Dir("/pics/")),
+		),
+	)
+
 	fmt.Println("Server started")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
