0cd56bf27a9ed944ca24a55a66675b1b2f837aa2
Keftcha <alban.dewilde@gmail.com>
2020-11-08 00:35:31 +0100

    Display directory name and pictures name

--- gallery.go
+++ gallery.go
@@ -3,13 +3,37 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 )
 
 func gallery(w http.ResponseWriter, r *http.Request) {
+	// Read files in the /pics directory
+	files, err := ioutil.ReadDir("/pics")
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+	}
+
+	// Separate directories and pictures
+	dirs, pics := make([]string, 0), make([]string, 0)
+	for _, file := range files {
+		// Check if it's a directory
+		if file.IsDir() {
+			dirs = append(dirs, file.Name())
+		} else {
+			pics = append(pics, file.Name())
+		}
+	}
+
+	// Make the map with infos to parse the template
+	info := make(map[string][]string)
+	info["dirs"] = dirs
+	info["pics"] = pics
+
+	// Format and send the template page
 	tpl, err := template.ParseFiles("pages/index.html")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	tpl.Execute(w, struct{}{})
+	tpl.Execute(w, info)
 }
